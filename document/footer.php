89e6eb91cd7811264dbae32e928b845a075e5a76
roman <75171315+Legenda1224@users.noreply.github.com>
2022-01-07 17:49:11 +0300

    Add files via upload

---
+++ document/footer.php
@@ -0,0 +1,43 @@
+<footer>
+    <div class="footer">
+        <div class="d_flex">
+            <div class="pokup">
+                <h3>
+                Покупателям
+                </h3>
+                <p>
+                <a href="indexoplata.php">Оплата</a><br>
+                <a href="indexdostavka.php">Доставка</a><br>
+                <a href="indexmagazin.php">Фирменные магазины партнеров</a><br>
+                <a href="indexgarantiya.php">Гарантия</a>
+                </p>
+            </div>
+            <div class="info">
+                <h3>
+                Информация
+                </h3>
+                <p>
+                <a href="indexonas.php">О нас</a><br>
+                <a href="indexcontact.php">Контакты</a><br>
+                </p>
+            </div>
+            <div class="contact">
+                <p>
+                8-800-551-25-99<br>
+                с 8:00 до 20:00 (МСК)<br>
+                Звонок по России бесплатный<br>
+            <div class="icons2">
+                <a href="#"><i class='bx bxl-vk'></i></a><br>
+                <a href="#"><i class='bx bxl-instagram-alt'></i></a>
+                <a href="#"><i class='bx bxl-telegram'></i></a>
+                <a href="#"><i class='bx bxl-facebook'></i></a>
+            </div>
+                </p>
+            </div>
+        </div>
+        <div class="pod_footer">
+            <p>Entropiq Интернет-магазин киберспортивный 2021<br>
+            <a href="indexpolitika.php">Политика конфиденциальности</a> | <a href="indexyslovie.php">Условия использования сайта</a></p>
+        </div>
+    </div>
+</footer>
---
+++ document/header.php
@@ -0,0 +1,68 @@
+<a name="magazin"></a>
+
+<!-- Бургер меню -->
+
+<div class="burger-menu">
+    <a href="" class="burger-menu_button">
+      <span class="burger-menu_lines"></span>
+    </a>
+    <nav class="burger-menu_nav">
+      <a href="index.php" class="burger-menu_link">Главная</a>
+      <a href="indexdostavka.php" class="burger-menu_link">Доставка</a>
+      <a href="indexoplata.php" class="burger-menu_link">Оплата</a>
+      <a href="indexgarantiya.php" class="burger-menu_link">Гарантия</a>
+      <a href="indexcontact.php" class="burger-menu_link">Контакты</a>
+      <div class="icon3">
+        <a href="kabinet.php"><i id="burger-user" class='bx bx-user'></i></a>
+        <a href="#"><i id="burger-cart" class='bx bx-cart'></i></a>
+    </div>
+    </nav>
+    <div class="burger-menu_overlay"></div>
+</div>
+
+
+<!--Шапка-->
+
+
+<div class="container">
+  <header>
+    <nav>
+        <ul class="nav_link">
+            <li><a href="indexdostavka.php">Доставка</a></li>
+            <li><a href="indexoplata.php">Оплата</a></li>
+            <li><a href="indexgarantiya.php">Гарантия</a></li>
+        </ul>
+    </nav>
+    <div id="search">
+        <input id="input" placeholder="Поиск..."/>
+        <button id="button"><i class='bx bx-search'></i></button>
+        <div class="spinner"><i class='bx bx-loader-circle'></i></div>
+    </div>
+    <div class="icon">
+        <a href="vxod.php"><i class='bx bx-user'></i></a>
+        <a href=""><i class='bx bx-cart'></i></a>
+    </div>
+  </header>
+</div>
+
+
+<div class="pod_container">
+  <div class="content">
+    <div>
+        <a href="index.php"><img class="logo" src="/img/logo.png" alt="logo"></a>
+    </div>
+    <div id="text">
+        <p>
+            Магазин с самым большим ассортиментом товаров<br> для геймеров. 
+            Работаем с 2013 года! Доставляем<br> ежедневно. Без выходных и праздничных дней.
+        </p>
+    </div>
+    <div class="telefon">
+        <span>
+            <a href="tel:8 (800) 201-88-01">8 (800) 201-88-01</a><br>
+            Россия
+        </span>
+    </div>
+</div>
+
+        
